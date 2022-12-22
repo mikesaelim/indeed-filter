4f02c4f8421dfa584a94f6f9fb6830ce43d2392e
Mike Saelim <mike.saelim@gmail.com>
2022-12-22 15:30:10 -0600

    Show job counts

--- frontend/src/JobsPage.js
+++ frontend/src/JobsPage.js
@@ -5,17 +5,23 @@ import JobList from "./JobList";
 
 function JobsPage() {
   const [jobs, setJobs] = useState([]);
+  const [totalJobCount, setTotalJobCount] = useState(null);
 
   useEffect(() => {
     // TODO: loading overlay?
     Api.listJobs()
-      .then(results => setJobs(results.jobs))
+      .then(results => {
+        setJobs(results.jobs);
+        setTotalJobCount(results.totalCount);
+      })
       .catch(err => console.log(err));
   }, []);
 
   return (
     <div className="JobsPage">
-      <div className="m-4 text-center">insert header here</div>
+      <div className="m-4">
+        <h4>Showing {jobs.length} of {totalJobCount} jobs...</h4>
+      </div>
       <JobList jobs={jobs} />
     </div>
   );
